65a9092fa25cd795211d48731d80d0abd80094a3
Ken <edu@kenko.science>
2021-12-13 23:19:11 +0800

    init upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Form } from 'react-bootstrap'
+import MathJax from "mathjax3-react";
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+import "draft-js/dist/Draft.css"
+import "draftail/dist/draftail.css"
+import { DraftailEditor, BLOCK_TYPE, INLINE_STYLE } from "draftail"
+
 
 function App() {
+  const [qText, setQText] = useState("");
+
+  const initial = JSON.parse(sessionStorage.getItem("draftail:content"))
+
+  const onSave = (content) => {
+    console.log("saving", content)
+    sessionStorage.setItem("draftail:content", JSON.stringify(content))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <Container>
+        <h1>Testing</h1>
+        <Row>
+          <Col>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              onChange={e => { setQText(e.target.value) }}
+              value={qText}
+              spellCheck={false}
+            />
+            <DraftailEditor
+              rawContentState={initial || null}
+              onSave={onSave}
+              blockTypes={[
+                { type: BLOCK_TYPE.HEADER_THREE },
+                { type: BLOCK_TYPE.UNORDERED_LIST_ITEM },
+              ]}
+              inlineStyles={[{ type: INLINE_STYLE.BOLD }, { type: INLINE_STYLE.ITALIC }]}
+            />
+
+          </Col>
+          <Col>
+            <MathJax.Provider
+              options={{
+                tex: {
+                  inlineMath: [['$', '$'], ['\\(', '\\)']],
+                  processEscapes: true
+                }
+              }}
+            >
+              <MathJax.Html html={qText} />
+            </MathJax.Provider>
+          </Col>
+        </Row>
+      </Container>
+    </div >
   );
 }
 
